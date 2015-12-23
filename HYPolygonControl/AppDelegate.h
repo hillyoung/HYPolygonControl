4fc0c7553089f06cace9cd68264d70d753c4c3dc
yanghaha <389181073@qq.com>
2015-12-23 16:42:32 +0800

    Initial Commit

---
+++ HYPolygonControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYPolygonControl
+//
+//  Created by yanghaha on 15/12/23.
+//  Copyright © 2015年 yanghaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYPolygonControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HYPolygonControl
+//
+//  Created by yanghaha on 15/12/23.
+//  Copyright © 2015年 yanghaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
